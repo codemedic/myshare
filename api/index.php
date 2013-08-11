50299d83193d968e1a975119d54a0b57486c1fee
Dino Korah <dckorah@gmail.com>
2013-08-11 19:54:23 -0400

    More deps added for twig
    also some example code for slim-fw

--- api/index.php
+++ api/index.php
@@ -1,3 +1,22 @@
 <?php
 
 require 'vendor/autoload.php';
+
+$app = new \Slim\Slim(
+    array(
+        'debug' => true,
+        'view' => new \Slim\Views\Twig()
+    )
+);
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => __DIR__.'/cache'
+);
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+$app->run();
